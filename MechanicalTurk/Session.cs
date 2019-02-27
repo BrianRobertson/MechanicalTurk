7c793fdd2f408c9e60757e653ca684e93d4e0ce7
BrianRobertson <briaonrobertson@gmail.com>
2019-02-27 13:05:21 -0600

    Added all relevent code from previous Mechanical Turk project.

--- MechanicalTurk/Session.cs
+++ MechanicalTurk/Session.cs
@@ -12,12 +12,15 @@ namespace MechanicalTurk
         private User user2 = new User();
         public void SetUpSession()
         {
-            Console.WriteLine("Hello World! My name is Mechanical Turk");
+            Console.WriteLine("Hello World! \nMy name is Mechanical Turk");
             user1.SetName();
         }
         public void RunSession()
         {
             Console.WriteLine(user1.name + ", I am happy to report the session is now running.");
+            Console.WriteLine(user1.name + ", What are your thoughts on what to do next? Type them here please.");
+            string playerThoughts = Console.ReadLine();
+            Console.WriteLine("Your thoughts about " + playerThoughts + " seem good to me.");
         }
     }
 }
